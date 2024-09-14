63a09b3f5300fe160a089255bf9acebe54452890
matrix05-c <caddyrasolonjatovo@gmail.com>
2024-09-14 15:18:02 +0300

    Message décrivant les modifications apportées

--- ProjetL1-main/GUI/Window.cpp
+++ ProjetL1-main/GUI/Window.cpp
@@ -2,6 +2,7 @@
 #include "ui_Window.h"
 #include <QPixmap>
 #include <QDebug>
+#include <QDir>
 
 Window::Window(QWidget *parent)
     : QMainWindow(parent)
@@ -10,7 +11,7 @@ Window::Window(QWidget *parent)
     ui->setupUi(this);
 
     //ty le logo an le izy.......................................................
-    QPixmap pixmap("D:\\tuto\\ProjetL1-main\\GUI\\res\\cop_logo.png");
+    QPixmap pixmap("..\\GUI\\res\\cop_logo.png");
 
     if(pixmap.isNull()){
         qDebug() <<"erreur";
@@ -18,25 +19,50 @@ Window::Window(QWidget *parent)
     ui->label->setPixmap(pixmap);
 
     //ty le logo faharoa eo @ le accueil...............
-    QPixmap pixel("D:\\tuto\\ProjetL1-main\\GUI\\res\\home.svg");
+    QPixmap pixel("..\\GUI\\res\\home.svg");
     ui->label_4->setPixmap(pixel);
 
     //logo fahatelo eo @ le nouveau.............................
-    QPixmap pixmap2("D:\\tuto\\ProjetL1-main\\GUI\\res\\user-plus.svg");
+    QPixmap pixmap2("..\\GUI\\res\\user-plus.svg");
     ui->label_8->setPixmap(pixmap2);
 
     //logo fahaefatra eo @ le liste...................................
-    QPixmap pixel2("D:\\tuto\\ProjetL1-main\\GUI\\res\\list.svg");
+    QPixmap pixel2("..\\GUI\\res\\list.svg");
     ui->label_10->setPixmap(pixel2);
 
     //logo eo @le recherche.............
-    QPixmap pixmapsearch("D:\\tuto\\ProjetL1-main\\GUI\\res\\search.svg");
+    QPixmap pixmapsearch("../GUI/res/search.svg");
     ui->label_26->setPixmap(pixmapsearch);
 
+
     //sary eo @le page d'accueil juste noforoporoniko fotsiny.....
-    QPixmap schema1("D:\\tuto\\ProjetL1-main\\GUI\\res\\sary1.jpg");
-    QPixmap schema2("D:\\tuto\\ProjetL1-main\\GUI\\res\\sary2.jpg");
-    ui->label_29->setPixmap(schema1);
+    QPixmap schema1("../GUI/res/sary1.jpg");
+   QPixmap schema1scaled = schema1.scaled(800, 1000, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label_5->setPixmap(schema1scaled);
+
+    //..
+    QPixmap schema2("../GUI/res/sary3.jpg");
+   QPixmap schema2scaled = schema2.scaled(800, 1000, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label_7->setPixmap(schema2scaled);
+
+    //..
+    QPixmap schema3("../GUI/res/sary2.jpg");
+   QPixmap schema3scaled = schema3.scaled(800, 1000, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label_9->setPixmap(schema3scaled);
+
+    //...
+    QPixmap schema4("../GUI/res/sary5.jpg");
+   QPixmap schema4scaled = schema4.scaled(800, 1000, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label_25->setPixmap(schema4scaled);
+
+    //....
+    QPixmap schema5("../GUI/res/sary6.png");
+    QPixmap schema5scaled = schema5.scaled(100, 100, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->label_29->setPixmap(schema5scaled);
+
+
+
+
 
 
 
